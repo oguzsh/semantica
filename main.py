ae78ba0589f6812fe9bdfea1c2f74eaf8f832d10
Oguzhan Ince <oguzhan824@gmail.com>
2025-05-06 13:28:12 +0300

    Implement initial FastAPI application with file upload functionality and health check endpoint

---
+++ main.py
@@ -0,0 +1,34 @@
+import os.path
+from typing import Annotated
+
+from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
+
+from item import Item
+
+app = FastAPI()
+
+file_save_path = os.path.join(os.getcwd(), 'temp')
+
+# Routes
+@app.get("/")
+async def read_root():
+  return {"message": "Hello World"}
+
+@app.get('/health_check')
+async def health_check():
+  return {"status": "ok"}
+
+@app.post('/upload')
+async def upload_file(file: UploadFile):
+  try:
+    file_path = os.path.join(file_save_path, f"{file.filename}")
+    if not os.path.exists(file_save_path):
+      os.makedirs(file_save_path)
+
+    with open(file_path, 'wb') as f:
+      f.write(await file.read())
+
+    return {"file_name": file.filename}
+  except Exception as e:
+    return {"message": e.args}
