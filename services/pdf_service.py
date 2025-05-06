ff89f1e1d063652d7b7c67b2c74feeffa581acfc
Oguzhan Ince <oguzhan824@gmail.com>
2025-05-06 15:49:07 +0300

    Enhance PdfService by adding metadata to document chunks and improving chunk tracking during PDF processing

--- services/pdf_service.py
+++ services/pdf_service.py
@@ -1,23 +1,36 @@
+from datetime import datetime
+import os
+from langchain.docstore.document import Document
 from langchain_text_splitters import MarkdownTextSplitter
 import pymupdf4llm
 
 class PdfService:
   def __init__(self, file_path: str):
     self.file_path = file_path
+    self.filename = os.path.basename(file_path)
 
   def process_pdf(self):
     parsed_pdf = self.__convert_to_markdown()
     chunks = self.__split_by_chunks(parsed_pdf)
-
     return chunks
 
   def __convert_to_markdown(self):
-    parsed_pdf = pymupdf4llm.to_markdown(self.file_path)
-    return parsed_pdf
+    return pymupdf4llm.to_markdown(self.file_path)
 
   def __split_by_chunks(self, parsed_pdf: str, chunk_size: int = 500, chunk_overlap: int = 100):
-    splitter = MarkdownTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+      document = Document(
+          page_content=parsed_pdf,
+          metadata={
+              "source_file": self.filename,
+              "uploaded_at": datetime.now().isoformat(),
+              "file_path": self.file_path
+          }
+      )
 
-    splitted_data = splitter.create_documents([parsed_pdf])
+      splitter = MarkdownTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+      splitted_documents = splitter.split_documents([document])
 
-    return splitted_data
+      for idx, doc in enumerate(splitted_documents):
+          doc.metadata["chunk_id"] = idx # For tracking
+
+      return splitted_documents
