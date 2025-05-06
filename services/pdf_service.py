e9700894f5669f98e5e2c8d3c25db128f9dab8e4
Oguzhan Ince <oguzhan824@gmail.com>
2025-05-06 14:21:44 +0300

    Refactor PdfService to improve PDF processing by separating conversion and chunking logic with langChain

--- services/pdf_service.py
+++ services/pdf_service.py
@@ -1,9 +1,23 @@
+from langchain_text_splitters import MarkdownTextSplitter
 import pymupdf4llm
 
 class PdfService:
   def __init__(self, file_path: str):
     self.file_path = file_path
 
-  def convert_to_markdown(self):
-    parsed_pdf = pymupdf4llm.to_markdown(self.file_path, page_chunks=True)
+  def process_pdf(self):
+    parsed_pdf = self.__convert_to_markdown()
+    chunks = self.__split_by_chunks(parsed_pdf)
+
+    return chunks
+
+  def __convert_to_markdown(self):
+    parsed_pdf = pymupdf4llm.to_markdown(self.file_path)
     return parsed_pdf
+
+  def __split_by_chunks(self, parsed_pdf: str, chunk_size: int = 500, chunk_overlap: int = 100):
+    splitter = MarkdownTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+
+    splitted_data = splitter.create_documents([parsed_pdf])
+
+    return splitted_data
